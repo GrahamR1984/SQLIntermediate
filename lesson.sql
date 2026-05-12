e6767a7f6ba6b1596d4bc578efb98754514b5f4c
GrahamR1984 <graham.roberts10@nhs.net>
2026-05-12 11:13:55 +0000

    Refactor SQL queries to enhance clarity and add new CASE statements for HospitalLocation and WardType

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,27 @@
-/*
-Example lesson stub
-*/
-
 SELECT
-	*
+  ps.PatientId
+ 
+  ,ps.AdmittedDate
+ 
+  ,ps.DischargeDate
+ 
+  ,ps.Hospital
+ 
+  ,ps.Ward
+ 
+  ,DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) + 1 AS LengthOfStay
+ 
+  ,DATEADD(month, 3, ps.DischargeDate) AS AppointmentDate
+ 
+  ,DATEADD(week, -2, ps.AdmittedDate) AS ReminderDate
 FROM
-	PatientStay ps ;
+  PatientStay ps
+WHERE
+ --ps.Hospital IN ( 'Oxleas', 'PRUH' )
+ --AND 
+ ps.AdmittedDate BETWEEN '2024-02-01' AND '2024-02-28'
+  --AND ps.Ward LIKE '%Surgery'
+  AND ps.Ward LIKE 'D%'
+ORDER BY
+ ps.AdmittedDate DESC
+ , ps.PatientId DESC;
